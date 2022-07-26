2a2bdd3d39acc10130d6453494a95902f9953df3
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-07-26 10:56:30 -0300

    feat: license GPL added

--- main.php
+++ main.php
@@ -1,13 +1,13 @@
 <?php
 /*
-Plugin Name: WP Brazilian Fields Registry
+Plugin Name: Brazilian Fields Registry WP
 Version: 1.0.1
 Description: Adiciona os campos <strong>CPF/CNPJ</strong>(junto com tipo de pessoa), <strong>Telefone</strong>, <strong>Inscricao Estadual</strong> no cadastro de um usuário no WordPress. Utiliza os mesmos campos que o Brazilian Market on WooCommerce
 Author: Vinicius Blazius Goulart
 Author URI: https://github.com/ViniBGoulart
 Update URI: false
 
-Text Domain: wp-brazilian-fields-registry
+Text Domain: brazilian-fields-registry-wp
 Domain Path: /languages
 
 License: GPLv3
@@ -23,7 +23,7 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	\add_action( 'admin_notices', function () {
 		list( $plugin_name ) = \get_file_data( __FILE__, [ 'plugin name' ] );
 		$message = sprintf(
-			__( '%s can\'t be initialized because the Composer dependencies were not installed. Reinstall the plugin or run <code>composer install</code>.', 'wp-brazilian-fields-registry' ),
+			__( '%s can\'t be initialized because the Composer dependencies were not installed. Reinstall the plugin or run <code>composer install</code>.', 'brazilian-fields-registry-wp' ),
 			"<strong>$plugin_name</strong>"
 		);
 		echo "<div class='notice notice-error'><p>$message</p></div>";
