9a7b7782dbfcfb596f60ed43fcf436b23d1ba81e
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-07-27 20:52:35 -0300

    code update based in wordpress require

--- templates/admin-notice.php
+++ templates/admin-notice.php
@@ -1,4 +1,4 @@
 <?php // docs: https://developer.wordpress.org/reference/hooks/admin_notices/ ?>
-<div class="notice <?= v( $var['class'] ) ?>">
-	<p><?= v( $var['message'], 'default("Missing Message")', 'safe_html', 'raw' ) ?></p>
+<div class="notice <?php v( $var['class'] ) ?>">
+	<p><?php v( $var['message'], 'default("Missing Message")', 'safe_html', 'raw' ) ?></p>
 </div>
